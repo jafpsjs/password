59d7a6af535367126a442a7b56bb9b1ff89827bd
Joshua Avalon <git@avalon.sh>
2025-11-18 10:28:18 +0800

    doc: Add documentation

--- src/hash-password.ts
+++ src/hash-password.ts
@@ -33,18 +33,43 @@ function mapMemLimit(memLimit?: HashPasswordLimit): number {
 }
 
 export type HashPasswordOptions = {
+  /**
+   * For interactive, online operations, `interactive` provide a baseline for these two parameters.
+   * Alternatively, `moderate` can be used for more secure operations.
+   * For highly sensitive data and non-interactive operations, `sensitive` can be used.
+   */
   limit?: HashPasswordLimit;
+} | {
+  /**
+   * The maximum amount of RAM in bytes that the function will use.
+   * This number must be between {@link sodium.crypto_pwhash_MEMLIMIT_MIN} and {@link sodium.crypto_pwhash_MEMLIMIT_MAX}.
+   */
+  memLimit: number;
+
+  /**
+   * Represents the maximum amount of computations to perform.
+   * Raising this number will make the function require more CPU cycles to compute a key.
+   * This number must be between {@link sodium.crypto_pwhash_OPSLIMIT_MIN} and {@link sodium.crypto_pwhash_OPSLIMIT_MAX}.
+   */
+  opsLimit: number;
 };
 
+/**
+ * Create a password hash with a random salt.
+ *
+ * @param password Password to be hashed.
+ * @param opts Configuration for {@link sodium.crypto_pwhash_str_async}.
+ * @returns he generated hash, settings, salt, version and algorithm will be stored.
+ * @see https://libsodium.gitbook.io/doc/password_hashing/default_phf#password-storage
+ */
 export async function hashPassword(password: string, opts: HashPasswordOptions = {}): Promise<Uint8Array<ArrayBuffer>> {
-  const { limit } = opts;
   return await new Promise((resolve, reject) => {
     const outputBuffer = Buffer.alloc(sodium.crypto_pwhash_STRBYTES);
     sodium.crypto_pwhash_str_async(
       outputBuffer,
       Buffer.from(password),
-      mapOpsLimit(limit),
-      mapMemLimit(limit),
+      "opsLimit" in opts ? opts.opsLimit : mapOpsLimit(opts.limit),
+      "memLimit" in opts ? opts.memLimit : mapMemLimit(opts.limit),
       err => {
         if (err) {
           reject(err);
--- src/verify-password.ts
+++ src/verify-password.ts
@@ -1,5 +1,15 @@
 import sodium from "sodium-native";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { hashPassword } from "./hash-password.js";
 
+/**
+ * Verify a password hash generated with {@link hashPassword}.
+ *
+ * @param hash Buffer with generated with {@link hashPassword}.
+ * @param password Password to be verified.
+ * @returns `true` if the hash could be verified with the settings contained in str. Otherwise `false`.
+ * @see https://libsodium.gitbook.io/doc/password_hashing/default_phf#password-storage
+ */
 export async function verifyPassword(hash: Uint8Array<ArrayBuffer>, password: string): Promise<boolean> {
   return await new Promise((resolve, reject) => {
     sodium.crypto_pwhash_str_verify_async(
