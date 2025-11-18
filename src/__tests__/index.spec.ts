04db146546f4d9c8239890cf5bf1ca5945b49b20
Joshua Avalon <git@avalon.sh>
2025-11-18 10:40:58 +0800

    test: Add test for number limit

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,6 +1,7 @@
 import assert from "node:assert/strict";
 import { randomUUID } from "node:crypto";
 import { describe, it } from "node:test";
+import sodium from "sodium-native";
 import { hashPassword, verifyPassword } from "../index.js";
 
 describe("PasswordService", () => {
@@ -70,6 +71,26 @@ describe("PasswordService", () => {
   it("should hash and verify incorrect password  (sensitive)", async () => {
     const password = randomUUID();
     const hash = await hashPassword(password, { limit: "sensitive" });
+    const verifiedResult = await verifyPassword(hash, `${password}1`);
+    assert.equal(verifiedResult, false);
+  });
+
+  it("should hash and verify correct password (sensitive)", async () => {
+    const password = randomUUID();
+    const hash = await hashPassword(password, {
+      memLimit: sodium.crypto_pwhash_MEMLIMIT_MIN,
+      opsLimit: sodium.crypto_pwhash_OPSLIMIT_MIN
+    });
+    const verifiedResult = await verifyPassword(hash, password);
+    assert.equal(verifiedResult, true);
+  });
+
+  it("should hash and verify incorrect password  (sensitive)", async () => {
+    const password = randomUUID();
+    const hash = await hashPassword(password, {
+      memLimit: sodium.crypto_pwhash_MEMLIMIT_MIN,
+      opsLimit: sodium.crypto_pwhash_OPSLIMIT_MIN
+    });
     const verifiedResult = await verifyPassword(hash, `${password}1`);
     assert.equal(verifiedResult, false);
   });
